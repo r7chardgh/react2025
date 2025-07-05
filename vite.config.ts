06f514fd092a9c034092063c3face6c0b3ef844b
richard tsang <richard.tsang9641@gmail.com>
2025-07-05 19:06:44 +0800

    add vite plugin vercel

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import vercel from 'vite-plugin-vercel';
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -13,5 +14,5 @@ export default defineConfig({
       },
     }
   },
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), tailwindcss(),vercel()],
 })
