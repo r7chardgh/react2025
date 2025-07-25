7df981af828f93084c5431abf5bb634eeeb17283
richard tsang <richard.tsang9641@gmail.com>
2025-07-26 00:51:28 +0800

    modiify components - usecallback & usedebugvalue

--- src/components/hooks/UseCallback.tsx
+++ src/components/hooks/UseCallback.tsx
@@ -3,12 +3,12 @@ import Case from '../Case';
 import Tag from '../Tag';
 import { RiResetLeftFill } from "react-icons/ri";
 import { FaPlus } from 'react-icons/fa';
-import { IoMdInformationCircleOutline } from 'react-icons/io';
+import Reminder from '../Reminder';
 const UseCallback = () => {
     return (
         <div className='flex flex-col gap-4 items-center sm:items-start'>
             <Tag title='UseCallback' />
-            <p className=' bg-gray-600 px-2 py-1 rounded-sm flex justify-center items-start sm:items-center gap-1 text-left'><IoMdInformationCircleOutline className='mt-1 sm:mt-0' size={18}/>To see the result, please open browser console</p>
+            <Reminder title="To see the result, please open browser console" />
             <Case title='Case 1: test re-render performance with useCallback'>
                 <ParentComponentWithUseCallback />
             </Case>
@@ -30,7 +30,7 @@ const ParentComponentWithUseCallback = () => {
     return <div className='flex flex-col gap-2'>
         <div className='flex flex-col gap-2 items-start'>
             <p className='text-gray-600 text-sm'>parent button</p>
-            <button className='flex gap-2 justify-center items-center capitalize' onClick={() => setCount(count + 1)}><FaPlus/>count: {count}</button>
+            <button className='flex gap-2 justify-center items-center capitalize' onClick={() => setCount(count + 1)}><FaPlus />count: {count}</button>
         </div>
         <ChildComponent fn={cachedFn} />
     </div>
@@ -42,7 +42,7 @@ const ParentComponentWithOutUseCallback = () => {
     return <div className='flex flex-col gap-2'>
         <div className='flex flex-col gap-2 items-start'>
             <p className='text-gray-600 text-sm'>parent button</p>
-            <button className='flex gap-2 justify-center items-center capitalize' onClick={() => setCount(count + 1)}><FaPlus/>count: {count}</button>
+            <button className='flex gap-2 justify-center items-center capitalize' onClick={() => setCount(count + 1)}><FaPlus />count: {count}</button>
         </div>
         <ChildComponent fn={() => { setCount(0) }} />
     </div>
--- src/components/hooks/UseDebugValue.tsx
+++ src/components/hooks/UseDebugValue.tsx
@@ -1,7 +1,8 @@
 import { useDebugValue, useState } from 'react';
 import Case from '../Case'
 import Tag from '../Tag';
-import { IoMdInformationCircleOutline } from 'react-icons/io';
+import Reminder from '../Reminder';
+
 
 const UseDebugValue = () => {
     const [count, setCount] = useState(0);
@@ -9,7 +10,7 @@ const UseDebugValue = () => {
     return (
         <div className='flex flex-col gap-4 items-center sm:items-start'>
             <Tag title='UseDebugValue' />
-            <p className=' bg-gray-600 px-2 py-1 rounded-sm flex justify-center items-start gap-1 text-left'><IoMdInformationCircleOutline className='mt-1'/>To see the result, check in React Dev Tool</p>
+            <Reminder title='To see the result, check in React Dev Tool' />
             <Case title="Case 1: label count value to react dev tool">
                 <div className='flex flex-col items-start gap-2'>
                     <p className='text-sm text-gray-600'>button</p>
