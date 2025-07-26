7721fc54693a9c9a2aaefa50ca8e97d927b41689
richard tsang <richard.tsang9641@gmail.com>
2025-07-26 15:50:17 +0800

    update template

--- src/components/hooks/Template.tsx
+++ src/components/hooks/Template.tsx
@@ -1,11 +1,14 @@
 import Tag from '../Tag'
 import Case from '../Case'
+import Reminder from '../Reminder'
 
 const Template = () => {
     return (
         <div className='flex flex-col gap-4 items-center sm:items-start'>
             <Tag title='UseTemplate' />
+            <Reminder title="To see the result, please open browser console" />
             <Case title="Case 1: title">
+                <p className=" text-sm text-gray-600">input</p>
                 <div>example</div>
             </Case>
         </div>
